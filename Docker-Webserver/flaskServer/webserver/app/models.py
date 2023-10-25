e3963fe77f73ae4a166ef0883a2578e7e24f79d3
MaxGonzalezGT <m.gonzalez01@globant.com>
2023-10-25 00:34:54 -0300

    Adding all the models and schemas

--- Docker-Webserver/flaskServer/webserver/app/models.py
+++ Docker-Webserver/flaskServer/webserver/app/models.py
@@ -1,33 +1,228 @@
-from app import db
-from app import ma
+from app import db, ma
 import bcrypt
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    surname = db.Column(db.String(64))
-    email = db.Column(db.String(255))
-    password = db.Column(db.String(255))
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-    def __init__(self, name, surname, email, password):
-        self.name = name
-        self.surname = surname
-        self.email = email
-        self.password = self.set_password(bytes(password, 'UTF-8'))
+class CalculadorasPresupuestosClientes(db.Model):
+    __tablename__ = 'calculadoras_presupuestos_clientes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(255))
+    presupuestos_id = db.Column(db.Integer)
+    fecha = db.Column(db.Date, nullable=True)
+    email_cliente = db.Column(db.String(510), nullable=True)
+
+class Calculators(db.Model):
+    __tablename__ = 'calculators'
+    token = db.Column(db.String(40), primary_key=True)
+    url = db.Column(db.String(255), nullable=True)
+    ip = db.Column(db.String(255), nullable=True)
+    formula = db.Column(db.String(5100), nullable=True)
+    entity_ID = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(120), nullable=True)
+    activo = db.Column(db.Boolean, nullable=True)
+
+class Clientes(db.Model):
+    __tablename__ = 'clientes'
+    email = db.Column(db.String(255), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    telephone = db.Column(db.String(255), nullable=False)
+
+class EntidadesCalculadoras(db.Model):
+    __tablename__ = 'entidades_calculadoras'
+    id_entidad = db.Column(db.String(255), primary_key=True)
+    token = db.Column(db.String(255), primary_key=True)
+
+class Entities(db.Model):
+    __tablename__ = 'entities'
+    ID = db.Column(db.String(50), primary_key=True)
+    nombre = db.Column(db.String(255), nullable=True)
+    telefono = db.Column(db.String(255), nullable=True)
+    direccion = db.Column(db.String(250), nullable=True)
+    type = db.Column(db.String(255), nullable=True)
+    activo = db.Column(db.Boolean, nullable=False)
+    descripcion = db.Column(db.String(1024), nullable=False)
+
+class Etapa(db.Model):
+    __tablename__ = 'etapa'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(255), nullable=False)
+    tipo = db.Column(db.String(255), nullable=False)
+    titulo = db.Column(db.String(255), nullable=False)
+    subtitulo = db.Column(db.String(255), nullable=False)
+    posicion = db.Column(db.Integer, nullable=False)
+
+class EtapaData(db.Model):
+    __tablename__ = 'etapa_data'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    etapa_id = db.Column(db.Integer, nullable=False)
+    meta_key = db.Column(db.String(2550), nullable=False)
+    meta_value = db.Column(db.String(2550), nullable=False)
+    imagen = db.Column(db.LargeBinary, nullable=True)
+
+class EtapaOpcion(db.Model):
+    __tablename__ = 'etapa_opcion'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    etapa_id = db.Column(db.Integer, nullable=False)
+    meta_key = db.Column(db.String(2550), nullable=False)
+    meta_value = db.Column(db.String(2550), nullable=False)
+    imagen = db.Column(db.LargeBinary, nullable=True)
+
+class Logs(db.Model):
+    __tablename__ = 'logs'
+    date = db.Column(db.String(10), primary_key=True)
+    time = db.Column(db.String(8), primary_key=True)
+    procedure = db.Column(db.String(100), primary_key=True)
+    _in = db.Column(db.String(250), primary_key=True)
+    _out = db.Column(db.String(250), primary_key=True)
+
+class Presupuestos(db.Model):
+    __tablename__ = 'presupuestos'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    resultado = db.Column(db.Integer, nullable=True)
+    formula = db.Column(db.String(2550), nullable=True)
+    finalizado = db.Column(db.Boolean, nullable=True)
+
+class PresupuestosData(db.Model):
+    __tablename__ = 'presupuestos_data'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    presupuesto_id = db.Column(db.Integer, nullable=False)
+    meta_key = db.Column(db.String(2550), nullable=False)
+    meta_value = db.Column(db.String(2550), nullable=False)
+    etapa_id = db.Column(db.Integer, nullable=False)
+
+class Tokens(db.Model):
+    __tablename__ = 'tokens'
+    token = db.Column(db.String(255), primary_key=True)
+    vendido = db.Column(db.Boolean, nullable=False)
+    canjeado = db.Column(db.Boolean, nullable=False)
+    fechaFin = db.Column(db.Date, nullable=False)
+
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+    mail = db.Column(db.String(255), primary_key=True)
+    password = db.Column(db.String(255), nullable=False)
+    telefono = db.Column(db.String(14), nullable=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    ultimoAcceso = db.Column(db.Date, nullable=False)
+    ultimaIP = db.Column(db.String(15), nullable=False)
+    apellidos = db.Column(db.String(255), nullable=False)
+    activo = db.Column(db.Boolean, nullable=False)
+    imagen = db.Column(db.LargeBinary, nullable=True)
+
+    def __init__(self, mail, password, telefono, nombre, ultimoAcceso, ultimaIP, apellidos, activo, imagen):
+        self.mail = mail
+        self.password = self.set_password(password)
+        self.telefono = telefono
+        self.nombre = nombre
+        self.ultimoAcceso = ultimoAcceso
+        self.ultimaIP = ultimaIP
+        self.apellidos = apellidos
+        self.activo = activo
+        self.imagen = imagen
 
     def set_password(self, password):
         salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password, salt)
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
         return hashed
 
-class UserSchema(ma.SQLAlchemySchema):
+class UsersToCalculators(db.Model):
+    __tablename__ = 'users_to_calculators'
+    user_email = db.Column(db.String(255), db.ForeignKey('usuarios.mail'), primary_key=True)
+    calculator_token = db.Column(db.String(40), db.ForeignKey('calculators.token'), primary_key=True)
+
+class UsersToEntities(db.Model):
+    __tablename__ = 'users_to_entites'
+    user_email = db.Column(db.String(255), db.ForeignKey('usuarios.mail'), primary_key=True)
+    entity_id = db.Column(db.String(50), db.ForeignKey('entities.ID'), primary_key=True)
+    
+class CalculadorasPresupuestosClientesSchema(SQLAlchemyAutoSchema):
     class Meta:
-        model = User
+        model = CalculadorasPresupuestosClientes
 
-    name = ma.auto_field()
-    surname = ma.auto_field()
-    email = ma.auto_field()
+class CalculatorsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Calculators
+
+class ClientesSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Clientes
+
+class EntidadesCalculadorasSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = EntidadesCalculadoras
+
+class EntitiesSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Entities
+
+class EtapaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Etapa
+
+class EtapaDataSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = EtapaData
+
+class EtapaOpcionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = EtapaOpcion
+
+class LogsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Logs
+
+class PresupuestosSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Presupuestos
+
+class PresupuestosDataSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = PresupuestosData
+
+class TokensSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tokens
+
+class UsersToCalculatorsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UsersToCalculators
+
+class UsersToEntitiesSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UsersToEntities
+
+class UsuariosSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuarios
+
+    # Agregar un campo para la columna 'imagen'
+    imagen = fields.Field(data_key='imagen', serialize='bytes')
+
+    # Resto de los campos
+    mail = ma.auto_field()
     password = ma.auto_field()
+    telefono = ma.auto_field()
+    nombre = ma.auto_field()
+    ultimoAcceso = ma.auto_field()
+    ultimaIP = ma.auto_field()
+    apellidos = ma.auto_field()
+    activo = ma.auto_field()
 
-user_schema = UserSchema()
+
+calculadoras_presupuestos_clientes_schema = CalculadorasPresupuestosClientesSchema()
+calculators_schema = CalculatorsSchema()
+clientes_schema = ClientesSchema()
+entidades_calculadoras_schema = EntidadesCalculadorasSchema()
+entities_schema = EntitiesSchema()
+etapa_schema = EtapaSchema()
+etapa_data_schema = EtapaDataSchema()
+etapa_opcion_schema = EtapaOpcionSchema()
+logs_schema = LogsSchema()
+presupuestos_schema = PresupuestosSchema()
+presupuestos_data_schema = PresupuestosDataSchema()
+tokens_schema = TokensSchema()
+users_to_calculators_schema = UsersToCalculatorsSchema()
+users_to_entities_schema = UsersToEntitiesSchema()
+usuarios_schema = UsuariosSchema()
 
--- Docker-Webserver/flaskServer/webserver/app/routes/user.py
+++ Docker-Webserver/flaskServer/webserver/app/routes/user.py
@@ -1,54 +1,135 @@
 from flask import jsonify, request, current_app
 from app import db
 from app.routes import user_bp
-from app.models import User
-from app.models import user_schema
+from app.models import Usuarios
+from app.models import usuarios_schema
+from sqlalchemy.sql import text
+import bcrypt
 
 @user_bp.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
-    return user_schema.jsonify(users, many=True)
+    users = Usuarios.query.all()
+    return usuarios_schema.jsonify(users, many=True)
 
-@user_bp.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user is None:
-        return jsonify({"message": "User not found"}), 404
-    return user_schema.jsonify(user)
+@user_bp.route('/get_usuario', methods=['POST'], endpoint='get_usuario')
+def get_usuario():
+    # Parse the JSON data from the request
+    info = request.json
+
+    # Extract the 'usuario' field from the JSON data
+    usuario = info["usuario"]
+
+    # Define the input arguments for the stored procedure
+    args = [usuario]
+
+    try:
+        # Execute the stored procedure using SQLAlchemy
+        result = db.session.execute("CALL get_usuario(:email)", {"email": usuario}).fetchall()
+        result = [dict(row) for row in result]
+
+    except Exception as e:
+        result = []
+
+    # Return the result to the view
+    return jsonify({'result': result})
 
 @user_bp.route('/users', methods=['POST'])
 def create_user():
-    name = request.json['name']
-    surname = request.json['surname']
-    email = request.json['email']
-    password = request.json['password']
+    name = request.json['nombre']
+    apellidos = request.json['apellidos']
+    email = request.json['mail']
+    password = request.json['pass']
+    telefono = request.json['telefono']
+    ultimoAcceso = request.json['ultimoAcceso']
+    ultimaIP = request.json['ultimaIP']
+    activo = request.json['activo']
+    imagen = request.json['imagen']
 
-    user = User(name, surname, email, password)
+    user = Usuarios(
+        mail=email,
+        password=password,
+        telefono=telefono,
+        nombre=name,
+        ultimoAcceso=ultimoAcceso,
+        ultimaIP=ultimaIP,
+        apellidos=apellidos,
+        activo=activo,
+        imagen=imagen
+    )
+
     db.session.add(user)
     db.session.commit()
 
-    return user_schema.jsonify(user), 201
+    return usuarios_schema.jsonify(user), 201
 
 @user_bp.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
-    user = User.query.get(user_id)
+    user = Usuarios.query.get(user_id)
     if user is None:
         return jsonify({"message": "User not found"}), 404
 
-    user.name = request.json['name']
-    user.surname = request.json['surname']
-    user.email = request.json['email']
-    user.password = request.json['password']
+    user.mail = request.json.get('mail', user.mail)
+    user.password = request.json.get('pass', user.password)
+    user.telefono = request.json.get('telefono', user.telefono)
+    user.nombre = request.json.get('nombre', user.nombre)
+    user.ultimoAcceso = request.json.get('ultimoAcceso', user.ultimoAcceso)
+    user.ultimaIP = request.json.get('ultimaIP', user.ultimaIP)
+    user.apellidos = request.json.get('apellidos', user.apellidos)
+    user.activo = request.json.get('activo', user.activo)
+    user.imagen = request.json.get('imagen', user.imagen)
 
     db.session.commit()
-    return user_schema.jsonify(user)
+    return usuarios_schema.jsonify(user)
 
 @user_bp.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user = User.query.get(user_id)
+    user = Usuarios.query.get(user_id)
     if user is None:
         return jsonify({"message": "User not found"}), 404
 
     db.session.delete(user)
     db.session.commit()
-    return user_schema.jsonify(user)
+    return usuarios_schema.jsonify(user)
+
+
+@user_bp.route('/exist_usuario', methods=['POST'], endpoint='exist_usuario')
+def exist_usuario():
+    # Parse the JSON data from the request
+    info = request.json
+
+    # Extract the 'usuario' and 'pwd' fields from the JSON data
+    usuario = info['usuario']
+    pwd = info['pwd']
+
+    # Retrieve the user based on the provided email
+    user = Usuarios.query.filter_by(mail=usuario).first()
+
+    if user is not None and bcrypt.checkpw(pwd.encode('utf-8'), user.password.encode('utf-8')):
+        # User exists and the password is correct
+        return jsonify({'result': True})
+    else:
+        # User doesn't exist or the password is incorrect
+        return jsonify({'result': False})
+
+@user_bp.route('/getCalcsInfo', methods=['POST'], endpoint='getCalcsInfo')
+def get_calculators_info():
+    # Parse the JSON data from the request
+    info = request.json
+
+    # Extract the 'user' from the JSON data for the database query
+    user = info["user"]
+
+    # Create a SQL query to retrieve information about calculators
+    sql_query = text("""
+        SELECT c.name, c.url, c.ip, c.formula, c.entity_ID, c.name AS calculator_name, c.activo, c.token
+        FROM usuarios u
+        JOIN users_to_calculators utc ON u.mail = utc.user_email
+        JOIN calculators c ON c.activo = 1 AND utc.calculator_token = c.token
+        WHERE u.mail = :user
+    """)
+
+    # Execute the SQL query
+    result = db.session.execute(sql_query, {'user': user})
+
+    # Return the result as JSON
+    return jsonify({'result': [dict(row) for row in result]})     
